fbfa54a1d75914341615cf0923d78d135524127d
Takumi MIURA <mitaku1104@gmail.com>
2012-12-31 16:24:26 +0900

    Initial Commit

---
+++ lib/komachi-develop.rb
@@ -0,0 +1,7 @@
+require "komachi-develop/version"
+
+module Komachi
+  module Develop
+    # Your code goes here...
+  end
+end
---
+++ lib/komachi-develop/version.rb
@@ -0,0 +1,5 @@
+module Komachi
+  module Develop
+    VERSION = "0.0.1"
+  end
+end
