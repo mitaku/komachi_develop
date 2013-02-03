057057507efe574d2eca6ee68acf15ae475bf0f1
Takumi MIURA <mitaku1104@gmail.com>
2013-02-03 23:05:32 +0900

    Add pry caller_method command

---
+++ lib/komachi-develop/pry-extension/caller_method.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+KomachiDevelop::PryExtension.create_command "caller_method" do
+  def process
+    depth = args.first.to_i || 1
+    if /^(.+?):(\d+)(?::in `(.*)')?/ =~ caller(depth+1).first
+      file = Regexp.last_match[1]
+      line = Regexp.last_match[2].to_i
+      method = Regexp.last_match[3]
+      puts [file, line, method]
+    end
+  end
+end
