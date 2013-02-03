f2fe1866255a014fb14f68aeb27630550984d31c
Takumi MIURA <mitaku1104@gmail.com>
2013-02-03 23:54:57 +0900

    Load hirb

--- komachi-develop.gemspec
+++ komachi-develop.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |gem|
   gem.add_dependency 'binding_of_caller'
   gem.add_dependency 'tapp'
   gem.add_dependency 'awesome_print'
+  gem.add_dependency 'hirb-unicode'
 
   # gem.add_dependency 'view_source_map'
   # gem.add_dependency 'zeus'
---
+++ lib/komachi-develop/pry-extension/hirb.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'hirb'
+
+Hirb::View.instance_eval do
+  def enable_output_method
+    @output_method = true
+    @old_print = Pry.config.print
+    Pry.config.print = proc do |output, value|
+      Hirb::View.view_or_page_output(value) || @old_print.call(output, value)
+    end
+  end
+
+  def disable_output_method
+    Pry.config.print = @old_print
+    @output_method = nil
+  end
+end
+
+Hirb.enable
