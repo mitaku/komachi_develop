124ffa381572be0cac379d26088568f02410db9d
Takumi MIURA <mitaku1104@gmail.com>
2013-02-03 23:54:04 +0900

    Load tapp, awesome_print

--- lib/komachi-develop.rb
+++ lib/komachi-develop.rb
@@ -2,6 +2,8 @@ require "komachi-develop/version"
 require 'pry-rails'
 require 'komachi-develop/pry-extension'
 require 'better_errors'
+require 'awesome_print'
+require 'tapp'
 
 module KomachiDevelop
 end
